53625ccb750b871b89affe378e56417e809b61d4
eliasmurcray <eliasmurcray@gmail.com>
2024-03-17 03:14:12 -0700

    Update interface

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Jarvis
 Inspired by the Marvel Character Jarvis, this is an AI-powered assistant which can even execute commands on the user's computer and access certain realtime data. Written in C++.
-
-![Screenshot of Jarvis prompt completion](https://raw.githubusercontent.com/eliasmurcray/jarvis/mainline/jarvis-screenshot-1.png)
--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,26 @@ using std::cout;
 using std::cin;
 
 int main() {
-	string key = getEnvironmentVariable("GPT_KEY");
+	string key = getEnvironmentVariable("OPENAI_API_KEY");
 	if (key.empty()) {
-		cerr << "Error: Environment variable 'GPT_KEY' not found\n";
+		cerr << "Error: Environment variable 'OPENAI_API_KEY' not found\n";
 		return EXIT_FAILURE;
 	}
 
 	GPTClient client(key);
 	
+	cout << "Jarvis initialized. Type 'exit' to terminate the session.\n\n";
+	
 	string prompt;
-	getline(cin, prompt);
-	string res = client.getCompletion(prompt);
-	cout << res << "\n";
+	while (1) {
+		cout << "# ";
+		getline(cin, prompt);
+		if (prompt == "exit") break;
+		string res = client.getCompletion(prompt);
+		cout << "\n" << res << "\n\n";
+	}
+
+	cout << "Session terminated.\n";
 
 	return EXIT_SUCCESS;
 }
